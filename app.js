1037242317e0a1eed81f6b37c6cf7cc29a0572a2
Veoprato <75872616+Veoprato@users.noreply.github.com>
2021-12-23 08:43:30 -0500

    HTML Generation added

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => profileDataArr.forEach(profileItem => console.log(profileItem));
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+console.log(profileDataArgs)
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log("Portfolio complete! Check out the index.html to see the output!");
+});
+
+// const printProfileData = profileDataArr => profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// printProfileData(profileDataArgs);
